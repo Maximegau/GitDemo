8703f9a781b59dc6d68673c47150410ead9ec844
Maxime Gautier <maxime.gautier.w@gmail.com>
2019-02-12 13:41:51 -0500

    Added int

--- GithubDemo/Assets/BranchingDemo.cs
+++ GithubDemo/Assets/BranchingDemo.cs
@@ -8,6 +8,9 @@ public class BranchingDemo : MonoBehaviour
     {
         //Added Line Before
         Debug.Log("Test09");
+        int numbera = 10;
+        numbera++;
+        Debug.Log(gameObject.transform.position);
         //Add Line Middle
         Debug.Log("Line10");
     }
